bb42f402a24a75e02959e6da040633ddc5020f61
cntrkilril <shalkir2002@mail.ru>
2022-10-31 12:37:19 +0300

    init commit

--- src/components/QuizCreate/QuizCreateNumber.vue
+++
@@ -1,9 +0,0 @@
-<template >
-    <h2 class="text-base text-rhino-100 bg-rhino-600 px-2 leading-tight rounded-full"><span class="relative font-black mr-1">{{ number }}</span>вопрос </h2>
-</template>
-<script setup>
-    import {defineProps} from 'vue'
-    const props = defineProps({
-        number: Number
-    })
-</script>
